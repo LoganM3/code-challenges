50236508b8e620096b972636d5e8b95c831a1e57
LoganM3 <mccalleylogan@gmail.com>
2022-07-16 12:16:36 -0400

    fizz buzz array

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,3 +2,16 @@
 // The task is simple: Print integers 1 to N, but print "Fizz" 
 // if an integer is divisible by 3, "Buzz" if an integer is divisible by 5, and "FizzBuzz" 
 // if an integer is divisible by both 3 and 5.
+
+function FizzBuzz(x){
+    let results = []
+    for( let i=1; i <= x; i++){
+         if ( i % 15 == 0) results.push("FizzBuzz")
+        else if(i % 3 == 0) results.push("Fizz")
+        else if(i % 5 == 0) results.push("Buzz")
+        else results.push(i)
+    }
+     return results
+}
+let fizzResluts = FizzBuzz(15)
+console.log(fizzResluts)
