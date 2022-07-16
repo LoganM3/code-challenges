17eb90a41c08e9870ff593784b0c14f1beb92262
LoganM3 <mccalleylogan@gmail.com>
2022-07-16 13:47:10 -0400

    reverse string

--- reverseString.js
+++ reverseString.js
@@ -2,11 +2,25 @@
 
 
 
-function reverse(str){
-let arr = str.split('')
-let revArr = arr.reverse()
-let strRev = revArr.join('')
+// function reverse(str){
+// let arr = str.split('')
+// let revArr = arr.reverse()
+// let strRev = revArr.join('')
 
-    return strRev
-}
-console.log(reverse('The sky is blue'))
+//     return strRev
+// }
+// console.log(reverse('The sky is blue'))
+
+
+
+
+function reverse(str){
+    let arr = str.split('')
+    let revStr =''
+    for(let i = arr.length -1; i >= 0; i--){
+        revStr += arr[i]
+    }
+    return revStr
+     
+    }
+    console.log(reverse('The sky is blue'))
